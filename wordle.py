16d09eb8d9ea0d6c36fdf71cbf663d8aecc6f968
YAO-WEN-CHENG <iris891201@gmail.com>
2022-10-14 17:45:50 +0800

    get random answer

--- wordle.py
+++ wordle.py
@@ -1,9 +1,17 @@
+import random
+
 if __name__ == "__main__":
     # User input
     user_input = input()
 
     # define answer
-    answer = "apple"
+    word_file = open("words.txt","r")
+    dictionary_w = word_file.read().splitlines()
+    word_file.close()
+
+    # print(dictionary_w)
+    answer = random.sample(dictionary_w,1)[0]
+    print(answer)
 
     # compare user_input and answer
     for i in range (len(user_input)):
