d78f3edb9ccc62ee622a0c727fe635d72daabf5c
YAO-WEN-CHENG <iris891201@gmail.com>
2022-10-14 17:36:22 +0800

    complete answer and user input

--- wordle.py
+++ wordle.py
@@ -4,3 +4,12 @@ if __name__ == "__main__":
 
     # define answer
     answer = "apple"
+
+    # compare user_input and answer
+    for i in range (len(user_input)):
+        if user_input[i] == answer[i]:
+            print("A")
+        elif user_input[i] in answer:
+            print("B")
+        else:
+            print("X")
